5dcf93a517c4bd634367b87963a147fa8dd31806
Luis Chambilla Lanchipa <luistar3@gmail.com>
2021-10-07 12:56:25 -0500

    Update MarcaVehiculoControllers.php

--- MarcaVehiculoControllers.php
+++ MarcaVehiculoControllers.php
@@ -5,49 +5,65 @@ include_once('../Models/MarcaVehiculo.php');
 
 class MarcaVehiculoController extends MarcaVehiculo{
 
-    public function fncObtenerMarcaVehiculo($id){
+    public function fncObtenerMarcaVehiculo($inputs){
         $dtReturn = array();
         $businessMarcaVehiculo = new BusinessMarcaVehiculo();
-        $dtReturn = $businessMarcaVehiculo->fncObtenerBD($id);
+        $dtReturn = $businessMarcaVehiculo->fncObtenerBD($inputs->idMarcaVehiculo);
+        return $dtReturn;
+    }
+    public function fncListarMarcaVehiculo(){
+        $dtReturn = array();
+        $businessMarcaVehiculo = new BusinessMarcaVehiculo();
+        $dtListado = $businessMarcaVehiculo->fncListarBD();
+
+
+        foreach( $dtListado as $listado ){
+            $model = array();
+            $model['idMarcaVehiculo'] 	= $listado->idMarcaVehiculo;
+            $model['marca'] = $listado->marca;     			
+            array_push($dtReturn, $model);
+            unset($model);
+        }
+
         return $dtReturn;
     }
 
-    public function fncGuardarMarcaVehiculo($marca){
+    public function fncGuardarMarcaVehiculo($inputs){
         $dtReturn = array();
         $marcaVehiculo = new MarcaVehiculo();
         $businessMarcaVehiculo = new BusinessMarcaVehiculo();
-        $marcaVehiculo->marca = $marca;
+        $marcaVehiculo->marca = $inputs->marca;
         $dtReturn = $businessMarcaVehiculo->fncGuardarBD($marcaVehiculo);
         return $dtReturn;
     }
     
-    public function fncModificarMarcaVehiculo($idMarcaVehiculo, $marca){
+    public function fncModificarMarcaVehiculo($inputs){
         $dtReturn = array();
         $marcaVehiculo = new MarcaVehiculo();
         $businessMarcaVehiculo = new BusinessMarcaVehiculo();
-        $marcaVehiculo->idMarcaVehiculo = $idMarcaVehiculo;
-        $marcaVehiculo->marca = $marca;
+        $marcaVehiculo->idMarcaVehiculo = $inputs->idMarcaVehiculo;
+        $marcaVehiculo->marca = $inputs->marca;
         $dtMarcaVehiculo= $businessMarcaVehiculo->fncModificarBD($marcaVehiculo);
         $dtReturn = $this->fncObtenerMarcaVehiculo($dtMarcaVehiculo->idMarcaVehiculo);
         return $dtReturn;
     }
-    public function fncEliminarMarcaVehiculo($idMarcaVehiculo){
+    public function fncEliminarMarcaVehiculo($inputs){
         $businessMarcaVehiculo = new BusinessMarcaVehiculo();        
         $marcaVehiculo = new MarcaVehiculo();
-        $marcaVehiculo->idMarcaVehiculo = $idMarcaVehiculo;
+        $marcaVehiculo->idMarcaVehiculo = $inputs->idMarcaVehiculo;
         $businessMarcaVehiculo->fncEliminarBD($marcaVehiculo);
     }
 
 }
 
 
-$marcaVehiculo = new MarcaVehiculoController();
+// $marcaVehiculo = new MarcaVehiculoController();
 
-var_dump($marcaVehiculo->fncGuardarMarcaVehiculo('ferrari'));
-var_dump($marcaVehiculo->fncGuardarMarcaVehiculo('ferrari2'));
-var_dump($marcaVehiculo->fncObtenerMarcaVehiculo(32));
-var_dump($marcaVehiculo->fncObtenerMarcaVehiculo(33));
-var_dump($marcaVehiculo->fncModificarMarcaVehiculo(32,'ferrarimod'));
-$marcaVehiculo->fncEliminarMarcaVehiculo(33);
-var_dump($marcaVehiculo->fncObtenerMarcaVehiculo(33));
+// var_dump($marcaVehiculo->fncGuardarMarcaVehiculo('ferrari'));
+// var_dump($marcaVehiculo->fncGuardarMarcaVehiculo('ferrari2'));
+// var_dump($marcaVehiculo->fncObtenerMarcaVehiculo(32));
+// var_dump($marcaVehiculo->fncObtenerMarcaVehiculo(33));
+// var_dump($marcaVehiculo->fncModificarMarcaVehiculo(32,'ferrarimod'));
+// $marcaVehiculo->fncEliminarMarcaVehiculo(33);
+// var_dump($marcaVehiculo->fncObtenerMarcaVehiculo(33));
 ?>
